20e9556705c74a4d00358ceb659d37acf9421b8f
Jakub Janowski <jkjanowski@gmail.com>
2017-03-29 12:56:40 +0200

    split main xaml tabs to separate files

---
+++ MySQL_DAC/LogInTab.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MySQL_DAC {
+	/// <summary>
+	/// Interaction logic for LogInTab.xaml
+	/// </summary>
+	public partial class LogInTab: UserControl {
+		public LogInTab() {
+			InitializeComponent();
+		}
+
+		private void loginButton_Click(object sender, RoutedEventArgs e) {
+			Debug.WriteLine(usernameTextBox.Text);
+			Debug.WriteLine(passwordBox.SecurePassword);
+			Process.Start("Notepad.exe", usernameTextBox.Text, passwordBox.SecurePassword, "");
+		}
+	}
+}
--- MySQL_DAC/MainWindow.xaml.cs
+++ MySQL_DAC/MainWindow.xaml.cs
@@ -1,37 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MySQL_DAC {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-        }
-
-        private void loginButton_Click(object sender, RoutedEventArgs e) {
-            Debug.WriteLine(usernameTextBox.Text);
-            Debug.WriteLine(passwordBox.SecurePassword);
-			Process.Start("Notepad.exe", usernameTextBox.Text, passwordBox.SecurePassword, "");
-		}
-
-        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
